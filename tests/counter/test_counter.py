3805942e98187625999af4e473d931a1706d2ecd
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-04 10:55:23 -0300

    feat: 10 - Implemente um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+
+    word_counts = {
+        "python": 1639,
+        "javascript": 122,
+    }
+
+    for word, expected_count in word_counts.items():
+        assert (
+            count_ocurrences(path, word) == expected_count
+        ), f"Unexpected count for {word}. Expected: {expected_count}."
+        """Contagem inesperada para...""" """Esperada:"""
